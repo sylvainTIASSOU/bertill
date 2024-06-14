179e15bf609e6608840be08418fa1fd0d622d1c2
tiassousylvain6120@gmail.com <tiassousylvain6120@gmail.com>
2024-06-14 10:58:22 +0000

    v.1.0.9

--- helpers/db_helpers.js
+++ helpers/db_helpers.js
@@ -1,8 +1,9 @@
-var mysql = require('mysql')
-var config = require('config')
-var dbConfig = config.get('dbConfig')
+var mysql = require('mysql');
+var config = require('config');
+var dbConfig = config.get('dbConfig');
+var helper = require('./helpers');
+
 var db = mysql.createConnection(dbConfig);
-var helper = require('./helpers')
 
 if(config.has('optionalFeature.detail')) {
     var detail = config.get('optionalFeature.detail');
@@ -12,80 +13,75 @@ if(config.has('optionalFeature.detail')) {
 reconnect(db, () => {});
 
 function reconnect(connection, callback) {
-    helper.Dlog("\n New connection tentative ... (" + helper.serverYYYYMMDDHHmmss() + ")" )
+    helper.Dlog("\n New connection attempt ... (" + helper.serverYYYYMMDDHHmmss() + ")");
 
     connection = mysql.createConnection(dbConfig);
     connection.connect((err) => {
-        if(err) {
+        if (err) {
             helper.ThrowHtmlError(err);
 
             setTimeout(() => {
-                helper.Dlog('----------------- DB ReConnecting Error (' + helper.serverYYYYMMDDHHmmss() + ') ....................' );
-
+                helper.Dlog('----------------- DB Reconnecting Error (' + helper.serverYYYYMMDDHHmmss() + ') ....................');
                 reconnect(connection, callback);
-            }, 5 * 1000);
-        }else{
+            }, 5000);
+        } else {
             helper.Dlog('\n\t ----- New Connection established with database. -------');
             db = connection;
             return callback();
         }
-    } )
+    });
 
     connection.on('error', (err) => {
-        helper.Dlog('----- App is connection Crash DB Helper (' + helper.serverYYYYMMDDHHmmss() + ') -------');
+        helper.Dlog('----- App connection crashed in DB Helper (' + helper.serverYYYYMMDDHHmmss() + ') -------');
 
         if (err.code === "PROTOCOL_CONNECTION_LOST") {
             helper.Dlog("/!\\ PROTOCOL_CONNECTION_LOST Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-            reconnect(db, callback);
+            reconnect(connection, callback);
         } else if (err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT") {
             helper.Dlog("/!\\ PROTOCOL_ENQUEUE_AFTER_QUIT Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-            reconnect(db, callback);
+            reconnect(connection, callback);
         } else if (err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR") {
             helper.Dlog("/!\\ PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-            reconnect(db, callback);
+            reconnect(connection, callback);
         } else if (err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE") {
             helper.Dlog("/!\\ PROTOCOL_ENQUEUE_HANDSHAKE_TWICE Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-            reconnect(db, callback);
+            reconnect(connection, callback);
         } else if (err.code === "ECONNREFUSED") {
             helper.Dlog("/!\\ ECONNREFUSED Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-            reconnect(db, callback);
+            reconnect(connection, callback);
         } else if (err.code === "PROTOCOL_PACKETS_OUT_OF_ORDER") {
             helper.Dlog("/!\\ PROTOCOL_PACKETS_OUT_OF_ORDER Cannot establish a connection with the database. /!\\ (" + err.code + ")");
-            reconnect(db, callback);
-        }  else {
+            reconnect(connection, callback);
+        } else {
             throw err;
         }
-    })
-
+    });
 }
 
 module.exports = {
     query: (sqlQuery, args, callback) => {
-
-        if(db.state === 'authenticated' || db.state === "connected") {
+        if (db.state === 'authenticated' || db.state === "connected") {
             db.query(sqlQuery, args, (error, result) => {
                 return callback(error, result);
-            })
-        }else if ( db.state === "protocol_error" ) {
+            });
+        } else if (db.state === "protocol_error") {
             reconnect(db, () => {
                 db.query(sqlQuery, args, (error, result) => {
                     return callback(error, result);
-                })
-            })
-        }else{
-            reconnect(db, ()=>{
-                db.query(sqlQuery, args, (error, result ) => {
+                });
+            });
+        } else {
+            reconnect(db, () => {
+                db.query(sqlQuery, args, (error, result) => {
                     return callback(error, result);
-                } )
-            })
+                });
+            });
         }
-
     }
-}
+};
 
 process.on('uncaughtException', (err) => {
-
-    helper.Dlog('------------------------ App is Crash DB helper (' + helper.serverYYYYMMDDHHmmss() + ')-------------------------' );
+    helper.Dlog('------------------------ App crashed in DB helper (' + helper.serverYYYYMMDDHHmmss() + ') -------------------------');
     helper.Dlog(err.code);
     helper.ThrowHtmlError(err);
-})
+});
